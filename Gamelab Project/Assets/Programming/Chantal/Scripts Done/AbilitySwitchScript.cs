32ee020da6183b1c90382164780efa5c5817adee
Danial <danial.azimi.a@gmail.com>
2015-11-06 11:22:13 +0100

    Input manager update & ability switch edit

--- Gamelab Project/Assets/Programming/Chantal/Scripts Done/AbilitySwitchScript.cs
+++ Gamelab Project/Assets/Programming/Chantal/Scripts Done/AbilitySwitchScript.cs
@@ -10,10 +10,10 @@ using System.Collections.Generic;
 public class AbilitySwitchScript : MonoBehaviour {
 	
 	public int abilityNum;
-	public GameObject[] AbilityImgsObjArr;  //size 3
-	public Sprite[] abilityImgSprArr;		//size 5
-	public GameObject[] abilities;			//size 5
-	public bool[] abilityUnlocked;			//size 5
+	public GameObject[] AbilityImgsObjArr = new GameObject[3];  //size 3
+	public Sprite[] abilityImgSprArr = new Sprite[5];			//size 5
+	public GameObject[] abilities = new GameObject[5];			//size 5
+	public bool[] abilityUnlocked = new bool[5];				//size 5
 	public bool inNormalMode;
 	public ManaScript manaScr;
 	public float minManaNeed;
