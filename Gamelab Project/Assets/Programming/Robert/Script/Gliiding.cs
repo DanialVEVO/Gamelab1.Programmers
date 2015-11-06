0a38f3d2b50ca56bec14de7c61e24d7333acecab
Chievap <robert98neijmeijer@gmail.com>
2015-11-06 08:30:17 +0100

    My Scene Update

---
+++ Gamelab Project/Assets/Programming/Robert/Script/Gliiding.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gliiding : MonoBehaviour {
+
+	private Rigidbody 	rb;
+
+	public 	float 		oldMass;
+	public 	float 		newMass;
+
+	void Start (){
+		rb =  GetComponent<Rigidbody>();
+		oldMass = rb.mass;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton("Glide")){
+			rb.mass = newMass;
+		} 	
+		if(!Input.GetButton("Glide")){
+			if(rb.mass != oldMass){
+				rb.mass = oldMass;
+			}
+		}
+		//print(rb.mass);
+	}
+}
