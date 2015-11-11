2763c4d17839bf9c75966b11ec4f95e420f9dee1
Chievap <robert98neijmeijer@gmail.com>
2015-11-11 16:31:58 +0100

    Pickups done

---
+++ Gamelab Project/Assets/Programming/Robert/Script/Pickup/Pickup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Pickup : MonoBehaviour {
+
+	public int pickupCount;
+	public int maxPickupCount;
+	public Text pickupText;
+
+	void OnTriggerEnter (Collider collision){
+		if(collision.GetComponent<Collider>().tag == "Pickup"){
+			pickupCount ++;
+			//print("pickupCount");
+		}
+	}
+	void Update (){
+		pickupText.text = pickupCount + "/" + maxPickupCount;
+	}
+}
