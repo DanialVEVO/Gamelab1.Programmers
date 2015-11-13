3637c59b41f1ab7c2fd4f016f481e49414dde7ee
Danial <danial.azimi.a@gmail.com>
2015-11-13 08:46:10 +0100

    Particle Prefabs

---
+++ Gamelab Project/Assets/Programming/Danial/_Scripts in Progress/RandomParticleSelector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomParticleSelector : MonoBehaviour {
+
+	public ParticleSystem[] comicParticle = new ParticleSystem[3];
+	public int selectedInt;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void ParticleRandomizer (){
+		selectedInt = Random.Range(0,2);
+	}
+
+	void Test (){
+	}
+}
