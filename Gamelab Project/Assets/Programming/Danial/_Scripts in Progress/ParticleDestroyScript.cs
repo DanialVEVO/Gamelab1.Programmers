8ab5c26a767a13b5fb06bbefce2aec1d690e2e2a
Danial <danial.azimi.a@gmail.com>
2015-11-17 11:38:50 +0100

    Hierarchy update

--- Gamelab Project/Assets/Programming/Danial/_Scripts in Progress/ParticleDestroyScript.cs
+++ Gamelab Project/Assets/Programming/Danial/_Scripts in Progress/ParticleDestroyScript.cs
@@ -1,17 +1,23 @@
-﻿using UnityEngine;
+﻿/* [no code]
+ * Particle Destroyer Script
+ * Scripted by Chantal
+ */
+using UnityEngine;
 using System.Collections;
 
 public class ParticleDestroyScript : MonoBehaviour {
 
-	public Transform own;
-
 	// Use this for initialization
 	void Start () {
-//		own = gameObject.GetComponent;
+		ParticleDestroy();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void ParticleDestroy () {
+		Destroy(gameObject, GetComponent<ParticleSystem>().duration);
 	}
 }
