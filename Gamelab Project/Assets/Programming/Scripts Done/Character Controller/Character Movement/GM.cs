f5c82d4abf2aeb35d5682c73987cdee157f99ab6
Chievap <robert98neijmeijer@gmail.com>
2015-12-04 10:51:23 +0100

    CurserShow

--- Gamelab Project/Assets/Programming/Scripts Done/Character Controller/Character Movement/GM.cs
+++ Gamelab Project/Assets/Programming/Scripts Done/Character Controller/Character Movement/GM.cs
@@ -5,7 +5,20 @@ public class GM : MonoBehaviour {
 
 	public	float 	moveSpeed;
 	public	float 	forwardDis;
+
 	
+	void Update (){
+		if(Input.GetButtonDown("Jump")){
+			CursorShow();
+		}
+	}
+
+	void CursorShow (){
+		if(Cursor.visible == true){
+			Cursor.visible = false;
+		}
+		else{ Cursor.visible = true;}
+	}
 
 
 }
